54d0b617c738b6c85d52bdaadcda05a95a75ea4f
jnetosantana <144806481+jnetosantana@users.noreply.github.com>
2023-11-06 10:57:10 -0300

    exercício com numbers e Math

---
+++ aula14/index.js
@@ -0,0 +1,10 @@
+let num1 = 0.7
+let num2 = 0.1
+
+//console.log(num1.toString() + num2)
+num1 = ((num1 * 100) + (num2 * 100)) / 100
+num1 = ((num1 * 100) + (num2 * 100)) / 100
+num1 = ((num1 * 100) + (num2 * 100)) / 100
+
+console.log(num1)
+console.log(Number.isInteger(num1))
