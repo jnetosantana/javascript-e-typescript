e0a545c6e10df0b0e0830b41cfa52c5c5e662ab9
jnetosantana <144806481+jnetosantana@users.noreply.github.com>
2023-11-08 11:11:52 -0300

    estudando operadores lógicos

---
+++ aula22/script.js
@@ -0,0 +1,16 @@
+/*
+Operadores lógicos
+$$ -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+//const expressaoAnd =  true && true && true && true
+//const expressaoOr = false || true;
+
+//const usuario = 'Luiz';  form usuário digitou
+//const senha = '123456';  form usuário digitou
+
+
+//const vaiLogar = usuario === 'Luiz' && senha === '12345';
+
+console.log(vaiLogar);
