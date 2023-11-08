1d8113a8bbc2ff28edfabec74ffce60353bbf960
jnetosantana <144806481+jnetosantana@users.noreply.github.com>
2023-11-08 10:20:34 -0300

    operadores de comparação

---
+++ aula21/script.js
@@ -0,0 +1,11 @@
+/* 
+Operadores ded comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor) NÃO USAR
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) NÃO USAR
+!== diferente estrito (valor e tipo)
+*/
