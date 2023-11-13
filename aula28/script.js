548fce6db9028fd7e6a10821f5d602991e01a384
jnetosantana <144806481+jnetosantana@users.noreply.github.com>
2023-11-13 14:10:37 -0300

    .

--- aula28/script.js
+++ aula28/script.js
@@ -1,1 +1,5 @@
-const data = new Date();
+//const tresHoras = 60 * 60 * 3 * 1000;
+//const umDia = 60 * 60 * 24 * 1000;
+//const data = new Date(0 + tresHoras + umDia);
+const data = new Date(2023, 3, 20, 15, 14, 27, 500);
+console.log(data.toString())
