5022601d5d8b546174a5c0b904920ed59c3ba531
jnetosantana <144806481+jnetosantana@users.noreply.github.com>
2023-11-14 12:52:00 -0300

    estudando date

--- aula28/script.js
+++ aula28/script.js
@@ -1,5 +1,8 @@
 //const tresHoras = 60 * 60 * 3 * 1000;
 //const umDia = 60 * 60 * 24 * 1000;
 //const data = new Date(0 + tresHoras + umDia);
-const data = new Date(2023, 3, 20, 15, 14, 27, 500);
-console.log(data.toString())
+const data = new Date();
+console.log('Dia', data.getDate())
+console.log('Mês', data.getMonth())
+console.log('Ano', data.getFullYear())
+console.log('Dia da Semana', data.getDay())
